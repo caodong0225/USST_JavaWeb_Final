e68434e5b0d24860f5282e6fef22fc3d95a80ba8
jyzxc <867137498@qq.com>
2024-12-11 00:16:59 +0800

    feat: begin the project

---
+++ advertisement_management/src/main/java/usst/web/controller/AuthController.java
@@ -0,0 +1,19 @@
+package usst.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import usst.web.response.BaseResponse;
+
+/**
+ * @author jyzxc
+ * @since 2024-12-10
+ */
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @GetMapping("/login")
+    public BaseResponse login() {
+        return BaseResponse.makeResponse(200, "ok");
+    }
+}
