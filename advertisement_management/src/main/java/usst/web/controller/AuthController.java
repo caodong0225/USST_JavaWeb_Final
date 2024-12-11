c0529bd9b75ebe17dcae7298969b8c1e657a1f40
曹东 <867137498@qq.com>
2024-12-11 14:28:05 +0800

    feat: add the thymeleaf

--- advertisement_management/src/main/java/usst/web/controller/AuthController.java
+++ advertisement_management/src/main/java/usst/web/controller/AuthController.java
@@ -1,19 +1,22 @@
 package usst.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import usst.web.response.BaseResponse;
-
 /**
  * @author jyzxc
  * @since 2024-12-10
  */
-@RestController
+@Controller
 @RequestMapping("/auth")
 public class AuthController {
-    @GetMapping(value = "/login", produces = "application/json")
-    public BaseResponse login() {
-        return BaseResponse.makeResponse(200, "ok");
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";  // This will resolve to login.jsp
+    }
+    @GetMapping("/register")
+    public String register(Model model) {
+        return "register";
     }
 }
