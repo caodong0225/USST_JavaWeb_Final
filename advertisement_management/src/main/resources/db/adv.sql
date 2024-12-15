dc79835b53a3a60b2222707f0d3cb31257a8fca6
user6841 <2871066708@qq.com>
2024-12-15 13:47:36 +0800

    adv:advertisement

--- advertisement_management/src/main/resources/db/adv.sql
+++ advertisement_management/src/main/resources/db/adv.sql
@@ -1,34 +1,39 @@
-/*
- Navicat Premium Dump SQL
-
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 80038 (8.0.38)
- Source Host           : localhost:3306
- Source Schema         : adv
-
- Target Server Type    : MySQL
- Target Server Version : 80038 (8.0.38)
- File Encoding         : 65001
-
- Date: 11/12/2024 16:08:28
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+--
+-- Table structure for table `advertisement`
+--
+
+DROP TABLE IF EXISTS `advertisement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `advertisement` (
+                                 `ad_id` int NOT NULL,
+                                 `ad_name` varchar(45) DEFAULT NULL,
+                                 `ad_url` varchar(255) DEFAULT NULL,
+                                 `ad_image` varchar(255) DEFAULT NULL,
+                                 `ad_create_time` timestamp(4) NULL DEFAULT NULL,
+                                 `ad_start_time` timestamp(4) NULL DEFAULT NULL,
+                                 `ad_end_time` timestamp(4) NULL DEFAULT NULL,
+                                 `ad_use` tinyint DEFAULT NULL,
+                                 `ad_feature` varchar(255) DEFAULT NULL,
+                                 PRIMARY KEY (`ad_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
-  `password` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `user_pk2`(`username` ASC) USING BTREE
+                         `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
+                         `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
+                         `password` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
+                         `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                         `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                         PRIMARY KEY (`id`) USING BTREE,
+                         UNIQUE INDEX `user_pk2`(`username` ASC) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -36,12 +41,12 @@ CREATE TABLE `user`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `user_id` int NOT NULL COMMENT '用户id',
-  `role_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '角色名称',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `user_role_user_id_fk`(`user_id` ASC) USING BTREE,
-  CONSTRAINT `user_role_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+                              `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
+                              `user_id` int NOT NULL COMMENT '用户id',
+                              `role_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '角色名称',
+                              PRIMARY KEY (`id`) USING BTREE,
+                              INDEX `user_role_user_id_fk`(`user_id` ASC) USING BTREE,
+                              CONSTRAINT `user_role_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
