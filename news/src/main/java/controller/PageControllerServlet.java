962c5b1b4d54cc6ed28e734dbbe80fe4aaba50e0
骏杰 姚 <yjjsx03@126.com>
2024-12-27 18:18:04 +0800

    page

--- news/src/main/java/controller/PageControllerServlet.java
+++ news/src/main/java/controller/PageControllerServlet.java
@@ -8,23 +8,12 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/")
+@WebServlet("")
 public class PageControllerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String path = req.getRequestURI();
         System.out.println("访问路径: " + path);
-        if (path.endsWith("/login")) {
-            req.getRequestDispatcher("WEB-INF/loginPage.jsp").forward(req, resp);
-        } else if (path.endsWith("/register")) {
-            req.getRequestDispatcher("WEB-INF/registerPage.jsp").forward(req, resp);
-        } else {
-            req.getRequestDispatcher("WEB-INF/mainPage.jsp").forward(req, resp);
-        }
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
+        req.getRequestDispatcher("WEB-INF/mainPage.jsp").forward(req, resp);
     }
 }
