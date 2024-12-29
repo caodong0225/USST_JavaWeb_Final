b1fee26a69af09cf65e13a7e509c943439d8f910
qsmca <895615123@qq.com>
2024-12-29 19:48:17 +0800

    PAP API

---
+++ advertisement_management/src/main/java/usst/web/controller/PyController.java
@@ -0,0 +1,34 @@
+package usst.web.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@RestController
+public class PyController {
+    @Value("${python.path}") String pythonPath;
+    @GetMapping("/executePython")
+    public void   executePython(HttpServletRequest request, HttpServletRequest response) {
+        Process proc;
+
+
+        try {
+            proc = Runtime.getRuntime().exec(pythonPath);// 执行py文件
+            //用输入输出流来截取结果
+            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+            in.close();
+            proc.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
