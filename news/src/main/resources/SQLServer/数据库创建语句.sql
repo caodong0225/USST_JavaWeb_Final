cc9b58fe80510c05ab7f8b46915a4ac8f583ef0c
姚骏杰 <yjjsx03@126.com>
2024-12-30 21:22:12 +0800

    数据库创建语句

--- news/src/main/resources/SQLServer/数据库创建语句.sql
+++ news/src/main/resources/SQLServer/数据库创建语句.sql
@@ -1,12 +1,11 @@
 CREATE TABLE Users (
-	Id INT IDENTITY(1,1) PRIMARY KEY,
-	username NVARCHAR(20) NOT NULL,
+	username NVARCHAR(20) NOT NULL Primary Key,
 	password NVARCHAR(20) NOT NULL
 );
 CREATE TABLE UserTags (
-	userId int NOT NULL,
+	username NVARCHAR(20) NOT NULL,
 	tag NVARCHAR(10) NOT NULL,
 	clickCount int,  
-	PRIMARY KEY (userId, Tag),
-	Foreign Key (userId) references Users(id)
+	PRIMARY KEY (Username, Tag),
+	FOREIGN Key (username) REFERENCES Users(username)
 );
