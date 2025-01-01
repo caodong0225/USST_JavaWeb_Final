c1892d267c861591870af770de66fd6b3636c097
gfdfdxc <29912959+gfdfdxc@users.noreply.github.com>
2025-01-01 17:26:46 +0800

    enable cors

--- shopping_backend/app.py
+++ shopping_backend/app.py
@@ -3,15 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 from routes.goods import goods_bp
 from routes.orders import orders_bp
 from models import db
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.config.from_object('config.Config') # 一定是要config.Config！！！
 
 # 初始化数据库
 db.init_app(app)
+CORS(app)
 # 初始化数据库
 with app.app_context():
-    db.drop_all()
+    # db.drop_all()
     db.create_all()
     print("数据库表已创建！")
 # 注册蓝图
