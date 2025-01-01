8184c682037b6b3502e6c2b13358c883ca4a4306
gfdfdxc <29912959+gfdfdxc@users.noreply.github.com>
2025-01-01 21:04:13 +0800

    精简app的注册路由

--- shopping_frontend/src/App.tsx
+++ shopping_frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ProductDetail from './pages/ProductDetail';
-import Order from './pages/Order';
 import OrderQuery from './pages/OrderQuery';
 import NotFound from './pages/NotFound';
 
@@ -12,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/order" element={<Order />} />
         <Route path="/order-query" element={<OrderQuery />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
